3ca100659bcb00184a2b9720737f772d53e02a95
sergio-br0 <rodriguezbolvito4@gmail.com>
2023-09-07 23:08:37 -0600

    creacion de la funcion buscar y agregar evento al boton actualizar

--- src/js/mapa/index.js
+++ src/js/mapa/index.js
@@ -1,46 +1,116 @@
-import L from "leaflet"
+import { Dropdown } from "bootstrap";
+import Swal from "sweetalert2";
+import { validarFormulario, Toast, confirmacion} from "../funciones";
+import Datatable from "datatables.net-bs5";
+import { lenguaje  } from "../lenguaje";
+import L from "leaflet";
 
+const butonActualizar = document.getElementById("actualizar");
 
-const map = L.map('map', {
-    center: [15.525158, -90.32959],
-    zoom: 7,
+const map = L.map('mapa', {
+    center: [15.52,-90.32],
+    zoom : 5,
+    maxZoom : 15,
+    minZoom : 1,
+})
+const mapLayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 })
 
-const mapLayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',{
-    maxZoom: 19,
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map)
+const carreteraLayer = L.tileLayer('https://tile.waymarkedtrails.org/hiking/{z}/{x}/{y}.png', {
+	maxZoom: 18,
+	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | Map style: &copy; <a href="https://waymarkedtrails.org">waymarkedtrails.org</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+});
 
-const markerLayer = L.layerGroup(); 
-
-const icon = L.icon({
+const markerLayer = L.layerGroup();
+const icono = L.icon({
     iconUrl : './images/cit.png',
     iconSize : [35, 35]
 })
 
-const marker = L.marker([15.525158, -90.32959],{
-    icon
-}).addTo(markerLayer)
 
-var tooltip = L.tooltip()
-    .setLatLng([15.525158, -90.32959])
-    .setContent('Hello world!<br />This is a nice tooltip.')
-    .addTo(map);
-
-    var popup = L.popup()
-    .setLatLng([15.525158, -90.32959])
+L.circle([15.52,-90.32], {radius: 5000}).addTo(markerLayer);
+const popup = L.popup()
+    .setLatLng([15.52,-90.32])
     .setContent('<p>Hello world!<br />This is a nice popup.</p>')
 
-    marker.bindPopup(popup)
 
-    const coordenadas = [
-        [14.1, -90.5],
-        [14.3, -90.8],
-        [14.6, -90.9],
-        [15.0, -90.6],
-        [15.3, -90.7]
-      ];
+var latlngs = [
+    [45.51, -122.68],
+    [37.77, -122.43],
+    [34.04, -118.2]
+];
 
-var polygon = L.polygon(coordenadas, {color: 'red'}).addTo(map);
-L.circle([14.6, -90.9], {radius: 5000}).addTo(map);
+var polyline = L.polyline(latlngs, {color: 'red'}).addTo(markerLayer);
+
+
+mapLayer.addTo(map)
+carreteraLayer.addTo(map)
 markerLayer.addTo(map)
+
+map.on('click', (e) => {
+    console.log(e);
+    alert('diste click sobre el mapa')
+})
+
+
+const buscar = async () => {
+    const url = `/mapas1/API/mapa/buscar`;
+    const config = {
+        method: 'GET'
+    }
+
+    try {
+        const respuesta = await fetch(url, config);
+        const data = await respuesta.json();
+
+        console.log(data);
+
+        if (data && data.length > 0) {
+            data.forEach(registro => {
+                const latitud = parseFloat(registro.mapa_latitud);
+                const longitud = parseFloat(registro.mapa_longitud);
+
+                if (!isNaN(latitud) && !isNaN(longitud)) {
+                    const NuevoMarcador = L.marker([latitud, longitud], {
+                        icon: icono,
+                        draggable: true
+                    });
+
+                    const popup = L.popup()
+                        .setLatLng([latitud, longitud])
+                        .setContent(`<p>Nombre: ${registro.mapa_nombre}</p>
+                                  
+                        `);
+                   
+                    NuevoMarcador.bindPopup(popup);
+                    NuevoMarcador.addTo(markerLayer);
+                }
+            });
+        } else {
+            Toast.fire({
+                title: 'No se encontraron registros',
+                icon: 'info'
+            });
+        }
+
+    } catch (error) {
+        console.error('Error al cargar los datos desde la base de datos:', error);
+    }
+}
+
+
+butonActualizar.addEventListener("click", () => {
+
+    Toast.fire({
+        title: 'Actualizando datos...',
+        icon: 'info',
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 2000 
+    });
+
+    buscar();
+});
--- views/mapa/index.php
+++ views/mapa/index.php
@@ -6,7 +6,7 @@
 </div>
 
 <div class="row justify-content-center mb-3">
-    <div class="col-lg-11 p-3 border rounded" id="map" style="height: 60vh;min-height:auto;">
+    <div class="col-lg-11 p-3 border rounded" id="mapa" style="height: 60vh;min-height:auto;">
 </div>
 </div>
 <script src="<?= asset('build/js/mapa/index.js') ?>"></script>
