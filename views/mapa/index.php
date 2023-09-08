33e8d77c196a3b086397e67a8486d261bc60603a
sergio-br0 <rodriguezbolvito4@gmail.com>
2023-09-07 22:01:15 -0600

    Se agrego el boton actualizar a la vista del mapa

--- views/mapa/index.php
+++ views/mapa/index.php
@@ -1,7 +1,19 @@
-<h1 class="text-center">Prueba mapas</h1>
-<div class="row justify-content-center mb-3">
-    <div class="col-lg-11 p-3 border rounded" id="map" style="height: 60vh;min-height:auto;">
-
+<h1 class="text-center">MAPA CON COORDENADAS REGISTRADAS</h1>
+<div class="row justify-content-center">
+    <div class="col-lg-1 d-grid mb-lg-0 mb-2 mx-auto text-center">
+        <button class="btn btn-info btn-sm" id="actualizar" name="actualizar">ACTUALIZAR</button>
     </div>
 </div>
+
+<div class="row justify-content-center mb-3">
+    <div class="col-lg-11 p-3 border rounded" id="map" style="height: 60vh;min-height:auto;">
+</div>
+</div>
 <script src="<?= asset('build/js/mapa/index.js') ?>"></script>
+
+
+
+
+
+
+
